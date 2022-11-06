a941c72af6469c5fc13a62164eba1fb835e34fea
GUchechi <godswilluchendu7@gmail.com>
2022-11-06 14:26:16 +0100

    weather

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useEffect } from 'react';
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Abuja&units=imperial&appid=59f8b3c520ef6e0c41f2ca99bd65b7d6`
 
 const Weather = () => {
-  return (
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        axios.get(url).then((response) => {
+            setData(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    },[])
+
+    console.log(data)
+
+    if(!data) return null;
+
+    return (
     <div>
-      
+      <p>{data.main.temp.toFixed(0)}</p>
     </div>
   )
 }
