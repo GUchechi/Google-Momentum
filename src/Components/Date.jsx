e3099db82e5bc134c547c5d09f03ead03167332f
GUchechi <godswilluchendu7@gmail.com>
2022-11-06 18:13:54 +0100

    added date

--- src/Components/Date.jsx
+++ src/Components/Date.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const Date = () => {
+const DateToday = () => {
+
+    let today = new Date()
+    // console.log(today)
+    let date = today.getFullYear() + '/' + today.getMonth() + '/' + today.getDate()
   return (
     <div>
-      
+      <p>{date}</p>
     </div>
   )
 }
 
-export default Date
+export default DateToday
