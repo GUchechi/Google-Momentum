4fd83ac9fbed1c228f69a6003f900bee602c7658
GUchechi <godswilluchendu7@gmail.com>
2022-11-06 14:30:28 +0100

    added moment and timezone package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Time from './Components/Time';
 import Weather from './Components/Weather';
 
 function App() {
   return (
     <div className="App">
       <Weather />
+      <Time />
     </div>
   );
 }
